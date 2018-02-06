9d3657928a32f5fd3ef2f53718c353118ecea509
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2018-02-06 20:50:19 +0700

    fix some bugs

--- app/src/main/java/com/toong/androidcurrencyedittext/CurrencyEditText.java
+++ app/src/main/java/com/toong/androidcurrencyedittext/CurrencyEditText.java
@@ -9,18 +9,23 @@ import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.widget.EditText;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 /**
  * Created by PhanVanLinh on 25/07/2017.
  * phanvanlinh.94vn@gmail.com
+ *
+ * Some note <br/>
+ * <li>Always use locale US instead of default to make DecimalFormat work well in all language</li>
  */
-
 public class CurrencyEditText extends android.support.v7.widget.AppCompatEditText {
     private static String prefix = "VND ";
     private static final int MAX_LENGTH = 20;
     private static final int MAX_DECIMAL = 3;
-    private CurrencyTextWatcher mCurrencyTextWatcher = new CurrencyTextWatcher(this, prefix);
+    private CurrencyTextWatcher currencyTextWatcher = new CurrencyTextWatcher(this, prefix);
 
     public CurrencyEditText(Context context) {
         this(context, null);
@@ -37,30 +42,60 @@ public class CurrencyEditText extends android.support.v7.widget.AppCompatEditTex
         this.setFilters(new InputFilter[] { new InputFilter.LengthFilter(MAX_LENGTH) });
     }
 
+    @Override
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+        super.onFocusChanged(focused, direction, previouslyFocusedRect);
+        if (focused) {
+            this.addTextChangedListener(currencyTextWatcher);
+        } else {
+            this.removeTextChangedListener(currencyTextWatcher);
+        }
+        handleCaseCurrencyEmpty(focused);
+    }
+
+    /**
+     * When currency empty <br/>
+     * + When focus EditText, set the default text = prefix (ex: VND) <br/>
+     * + When EditText lose focus, set the default text = "", EditText will display hint (ex:VND)
+     */
+    private void handleCaseCurrencyEmpty(boolean focused) {
+        if (focused) {
+            if (getText().toString().isEmpty()) {
+                setText(prefix);
+            }
+        } else {
+            if (getText().toString().equals(prefix)) {
+                setText("");
+            }
+        }
+    }
+
     private static class CurrencyTextWatcher implements TextWatcher {
-        private final EditText mEditText;
-        private String prevString;
+        private final EditText editText;
+        private String previousCleanString;
         private String prefix;
 
         CurrencyTextWatcher(EditText editText, String prefix) {
-            mEditText = editText;
+            this.editText = editText;
             this.prefix = prefix;
         }
 
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            // do nothing
         }
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
+            // do nothing
         }
 
         @Override
         public void afterTextChanged(Editable editable) {
             String str = editable.toString();
             if (str.length() < prefix.length()) {
-                mEditText.setText(prefix);
-                mEditText.setSelection(prefix.length());
+                editText.setText(prefix);
+                editText.setSelection(prefix.length());
                 return;
             }
             if (str.equals(prefix)) {
@@ -69,10 +104,10 @@ public class CurrencyEditText extends android.support.v7.widget.AppCompatEditTex
             // cleanString this the string which not contain prefix and ,
             String cleanString = str.replace(prefix, "").replaceAll("[,]", "");
             // for prevent afterTextChanged recursive call
-            if (cleanString.equals(prevString) || cleanString.isEmpty()) {
+            if (cleanString.equals(previousCleanString) || cleanString.isEmpty()) {
                 return;
             }
-            prevString = cleanString;
+            previousCleanString = cleanString;
 
             String formattedString;
             if (cleanString.contains(".")) {
@@ -80,20 +115,16 @@ public class CurrencyEditText extends android.support.v7.widget.AppCompatEditTex
             } else {
                 formattedString = formatInteger(cleanString);
             }
-            mEditText.removeTextChangedListener(this); // Remove listener
-            mEditText.setText(formattedString);
-            if (formattedString.length() <= MAX_LENGTH) {
-                mEditText.setSelection(formattedString.length());
-            }else{
-                mEditText.setSelection(MAX_LENGTH);
-            }
-            mEditText.addTextChangedListener(this); // Add back the listener
+            editText.removeTextChangedListener(this); // Remove listener
+            editText.setText(formattedString);
+            handleSelection();
+            editText.addTextChangedListener(this); // Add back the listener
         }
 
         private String formatInteger(String str) {
             BigDecimal parsed = new BigDecimal(str);
-            DecimalFormat formatter;
-            formatter = new DecimalFormat(prefix + "#,###");
+            DecimalFormat formatter =
+                    new DecimalFormat(prefix + "#,###", new DecimalFormatSymbols(Locale.US));
             return formatter.format(parsed);
         }
 
@@ -102,9 +133,10 @@ public class CurrencyEditText extends android.support.v7.widget.AppCompatEditTex
                 return prefix + ".";
             }
             BigDecimal parsed = new BigDecimal(str);
-            DecimalFormat formatter;
-            // example patter VND #,###.00
-            formatter = new DecimalFormat(prefix + "#,###." + getDecimalPattern(str));
+            // example pattern VND #,###.00
+            DecimalFormat formatter = new DecimalFormat(prefix + "#,###." + getDecimalPattern(str),
+                    new DecimalFormatSymbols(Locale.US));
+            formatter.setRoundingMode(RoundingMode.DOWN);
             return formatter.format(parsed);
         }
 
@@ -113,27 +145,19 @@ public class CurrencyEditText extends android.support.v7.widget.AppCompatEditTex
          * For example: 10.2 -> return 0 | 10.23 -> return 00, | 10.235 -> return 000
          */
         private String getDecimalPattern(String str) {
-            int decimalCount = str.length() - 1 - str.indexOf(".");
-            String decimalPattern = "";
+            int decimalCount = str.length() - str.indexOf(".") - 1;
+            StringBuilder decimalPattern = new StringBuilder();
             for (int i = 0; i < decimalCount && i < MAX_DECIMAL; i++) {
-                decimalPattern += "0";
+                decimalPattern.append("0");
             }
-            return decimalPattern;
+            return decimalPattern.toString();
         }
-    }
 
-    @Override
-    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
-        super.onFocusChanged(focused, direction, previouslyFocusedRect);
-        if (focused) {
-            this.addTextChangedListener(mCurrencyTextWatcher);
-            if (getText().toString().isEmpty()) {
-                setText(prefix);
-            }
-        } else {
-            this.removeTextChangedListener(mCurrencyTextWatcher);
-            if (getText().toString().equals(prefix)) {
-                setText("");
+        private void handleSelection() {
+            if (editText.getText().length() <= MAX_LENGTH) {
+                editText.setSelection(editText.getText().length());
+            } else {
+                editText.setSelection(MAX_LENGTH);
             }
         }
     }
--- app/src/main/java/com/toong/androidcurrencyedittext/MainActivity.java
+++ app/src/main/java/com/toong/androidcurrencyedittext/MainActivity.java
@@ -2,12 +2,17 @@ package com.toong.androidcurrencyedittext;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
+
+    String a = "1756.6655";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BigDecimal parsed = new BigDecimal(a);
     }
 }
